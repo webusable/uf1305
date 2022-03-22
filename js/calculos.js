397340bdae71172740cbf676985c539921d7ced4
webusable <ifcd0110@gmx.com>
2022-03-22 09:29:00 +0100

    Trabajando con document.write

--- js/calculos.js
+++ js/calculos.js
@@ -16,24 +16,24 @@ function mostrarMultiplo(n, m) {
 }
 
 
-function mostrarFactorial( nf ) {
-    document.getElementById('td21').innerHTML = nf;
-    document.getElementById('td22').innerHTML = factorial( nf );
+function mostrarFactorial( n ) {
+    document.getElementById('td21').innerHTML = n;
+    document.getElementById('td22').innerHTML = factorial( n );
 }
 
 
-function mostrarResultados( op, n1, n2 = 0 ) {
+function mostrarResultados( op, n, m = 0 ) {
 
     switch (op) {
         case 'multiplo':
-            document.getElementById('td11').innerHTML = n1;
-            document.getElementById('td12').innerHTML = n2;
-            document.getElementById('td13').innerHTML = multiplosDeHasta(n1, n2);
+            document.getElementById('td11').innerHTML = n;
+            document.getElementById('td12').innerHTML = m;
+            document.getElementById('td13').innerHTML = multiplosDeHasta(n, m);
         break;
 
         case 'factorial' :
-            document.getElementById('td21').innerHTML = n1;
-            document.getElementById('td22').innerHTML = factorial( n1 );
+            document.getElementById('td21').innerHTML = n;
+            document.getElementById('td22').innerHTML = factorial( n );
         break;
         default:
     }
@@ -48,18 +48,44 @@ function multiplosDeHasta(n, m) {
         " son: ";
 
     multiplo = 0;
-    for (i = 1; multiplo < m; i++) {
+
+    // for (i = 1; multiplo < m; i++) {
+    //     multiplo = n * i;
+    //     mensaje = mensaje + multiplo + " ";
+    // }
+
+    // Inicialización de la variable contador
+    i = 1;
+
+    // Condición
+    while ( multiplo < m ) {
         multiplo = n * i;
         mensaje = mensaje + multiplo + " ";
+
+        // Incremento
+        i++;
     }
+
+
 
     return mensaje;
 }
 
-function factorial( nf ) {
+function factorial( n ) {
     var factorial = 1;
-    for ( i=1; i <= nf; i++ ) {
+    for ( i=1; i <= n; i++ ) {
         factorial = factorial * i;
     }
     return factorial;
+}
+
+function sumar( s1, s2 ) {
+    var suma = s1 + s2;
+    return suma;
+}
+
+
+function multiplicar( f1, f2 ){
+    var producto = f1 * f2;
+    return producto;
 }
